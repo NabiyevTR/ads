14a571a90284d0989c76c065f0d8d49b10d6689e
Timur <t.nabiyev@click-group.ru>
2023-04-19 15:11:38 +0300

    Array data structure

---
+++ array/src/main/java/ru/ntr/Timer.java
@@ -0,0 +1,27 @@
+package ru.ntr;
+
+public class Timer {
+    private static long startTime;
+    private static long durationNs;
+
+    public static void start() {
+        startTime = System.nanoTime();
+    }
+
+    public static void stop() {
+        durationNs = System.nanoTime() - startTime;
+    }
+
+    public static long getTimeInNs() {
+        return durationNs;
+    }
+
+    public static long getTimeInMs() {
+        return getTimeInNs() / 1_000_000;
+    }
+
+    public static float getTimeInS() {
+        return 1.0f * durationNs / 1_000_000_000;
+    }
+
+}
